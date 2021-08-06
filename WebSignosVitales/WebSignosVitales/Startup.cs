f90755909cbee31ff9621b1a6da0a85b0fbef31a
Daysi Galindo <daysi.galindo@acftechnologies.com>
2021-08-06 12:50:55 -0500

    NewAplication

---
+++ WebSignosVitales/WebSignosVitales/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSignosVitales.Startup))]
+namespace WebSignosVitales
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
