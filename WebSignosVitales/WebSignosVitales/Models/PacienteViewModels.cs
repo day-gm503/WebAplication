92c46232996a803d34c0e1932e84f6ceeece9d6f
Daysi Galindo <daysi.galindo@acftechnologies.com>
2021-08-11 22:03:43 -0500

    Create Controllers Pacientes

---
+++ WebSignosVitales/WebSignosVitales/Models/PacienteViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSignosVitales.Models
+{
+    public class PacienteViewModels
+    {
+        public int Idpaciente { get; set; }
+        public string Nombrepas { get; set; }
+        public string Sexo { get; set; }
+        public int Edad { get; set; }
+        public string Estadocivil { get; set; }
+        public Nullable<System.DateTime> Fecha_nac { get; set; }
+        public string Domicilio { get; set; }
+        public string telefono { get; set; }
+        public string correo { get; set; }
+    }
+}
